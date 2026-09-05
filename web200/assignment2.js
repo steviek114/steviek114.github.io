85741f430e0a273e44a5479eb6dc5028caeaa5be
Stephen K <ks81589@mail.harpercollege.edu>
2026-09-05 15:19:05 -0500

    Create assignment2.js

---
+++ web200/assignment2.js
@@ -0,0 +1,21 @@
+
+var studentName = "John";
+let favoriteNumber = 42;
+const learningJavaScript = true;
+
+
+
+
+document.getElementById("studentName").innerHTML = studentName;
+
+document.getElementById("favoriteNumber").innerHTML = favoriteNumber;
+
+document.getElementById("learningJavaScript").innerHTML = learningJavaScript;
+
+
+
+document.getElementById("message").innerHTML =
+    studentName + " has selected the number " + favoriteNumber +
+    " and it is " + learningJavaScript +
+    " that JavaScript is being learned. " +
+    "The scientific community is impressed.";
