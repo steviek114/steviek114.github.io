fc3f4f8fddf5eb4900c1688c26617d8c41646422
Stephen K <ks81589@mail.harpercollege.edu>
2026-09-12 14:02:29 -0500

    Update assignment3.js

--- web200/assignment3.js
+++ web200/assignment3.js
@@ -1,4 +1,4 @@
 let miles = 10;
 let kilometers = miles * 1.609;
 document.getElementById("originalValue").innerHTML = miles + "miles";
-document.getElementById("convertedlValue").innerHTML = kilometers + "kilometers";
+document.getElementById("convertedValue").innerHTML = kilometers + "kilometers";
